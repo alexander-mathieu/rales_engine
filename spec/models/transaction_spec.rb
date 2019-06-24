019af753945d6e10e9de8fba0722934698ba6a11
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-24 17:27:02 -0600

    Add validation specs to Transaction model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of :result }
+    it { should validate_presence_of :credit_card_number }
+  end
 end
