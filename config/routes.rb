02c524fb60d2a0dc38bf6e08f2b006e2a4126979
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-25 11:59:25 -0600

    Change Merchant routes to include .json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      get "merchants.json", to: 'merchants#index', as: :merchants_index
+      get "merchants/:id.json", to: 'merchants#show', as: :merchants_show
     end
   end
 end
