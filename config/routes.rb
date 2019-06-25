1e31d888a295b2b1492649b1a39df44f2044afe2
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-25 17:40:29 -0600

    Reorganize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get "/most_revenue", to: "most_revenue#index"
+      end
+
       resources :items, only: [:index, :show]
       resources :invoices, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :merchants, only: [:index, :show]
       resources :transactions, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
-
-      namespace :merchants do
-        get "/most_revenue", to: "most_revenue#index"
-      end
     end
   end
 end
