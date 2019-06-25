3e60b5044f091e6065aa8288fa45c81c58f1823e
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-25 12:03:31 -0600

    Update Merchant index spec expectations, add Merchant show spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,12 +4,24 @@ RSpec.describe "Merchants API" do
   it "delivers a list of all Merchants" do
     create_list(:merchant, 3)
 
-    get "/api/v1/merchants"
+    get "/api/v1/merchants.json"
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body)["data"]
 
-    expect(merchants["data"].count).to eq(3)
+    expect(merchants.count).to eq(3)
+  end
+
+  it "delivers a single Merchant by ID" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}.json"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)["data"]
+
+    expect(merchant["id"].to_i).to eq(id)
   end
 end
