4ad8c0b7c594c0c218a4c0965c919701b9c331c1
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-26 15:43:20 -0600

    Add unit_price conversation to ItemSerializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -6,4 +6,8 @@ class ItemSerializer
              :unit_price,
              :description,
              :merchant_id
+
+  attribute :unit_price do |item|
+   item.unit_price = (item.unit_price.to_f/100).to_s
+  end
 end
