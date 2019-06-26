8d406443dfca35558e9cc2195473de08b990ece0
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-26 12:48:36 -0600

    Refactor most_revenue and most_items class methods to use Transaction :successful scope

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,12 +4,10 @@ class Merchant < ApplicationRecord
 
   validates_presence_of :name
 
-  scope :default_sort, -> { order(name: :asc) }
-
   def self.most_revenue(quantity)
     select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue')
     .joins(invoices: [:invoice_items, :transactions])
-    .where(invoices: {status: "shipped"}, transactions: {result: "success"})
+    .merge(Transaction.successful)
     .group(:id)
     .order("total_revenue DESC")
     .limit(quantity)
@@ -18,9 +16,16 @@ class Merchant < ApplicationRecord
   def self.most_items(quantity)
     select('merchants.*, SUM(invoice_items.quantity) AS total_sold')
     .joins(invoices: [:invoice_items, :transactions])
-    .where(invoices: {status: "shipped"}, transactions: {result: "success"})
+    .merge(Transaction.successful)
     .group(:id)
     .order('total_sold DESC')
     .limit(quantity)
+  end
+
+  def self.total_revenue(date)
+    select('SUM(invoice_items.quantity * invoice_items.unit_price)')
+    .joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.unscoped.successful)
+    .where("transactions.created_at BETWEEN ? AND ?", "#{date} 00:00:00", "#{date} 23:59:59")
   end
 end
