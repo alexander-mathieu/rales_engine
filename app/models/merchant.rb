d84c5e6a6ba887fef6d4515071ae8bc4af09c218
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-25 18:59:24 -0600

    Add most_revenue class method to Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,13 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   validates_presence_of :name
+
+  def self.most_revenue(quantity)
+    select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue')
+    .joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: "success"}, invoices: {status: "shipped"})
+    .group(:id)
+    .order("total_revenue DESC")
+    .limit(quantity)
+  end
 end
