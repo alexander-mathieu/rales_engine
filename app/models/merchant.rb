878a572aea4d3ccfd63efe30f6a204d4a67c4eae
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-26 17:24:08 -0600

    Refactor most_items/most_revenue Merchant class methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,7 +5,7 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   def self.most_revenue(quantity)
-    select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue')
+    select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
     .joins(invoices: [:invoice_items, :transactions])
     .merge(Transaction.successful)
     .group(:id)
@@ -14,18 +14,11 @@ class Merchant < ApplicationRecord
   end
 
   def self.most_items(quantity)
-    select('merchants.*, SUM(invoice_items.quantity) AS total_sold')
+    select("merchants.*, SUM(invoice_items.quantity) AS total_sold")
     .joins(invoices: [:invoice_items, :transactions])
     .merge(Transaction.successful)
     .group(:id)
     .order('total_sold DESC')
     .limit(quantity)
-  end
-
-  def self.total_revenue(date)
-    select('SUM(invoice_items.quantity * invoice_items.unit_price)')
-    .joins(invoices: [:invoice_items, :transactions])
-    .merge(Transaction.unscoped.successful)
-    .where("transactions.created_at BETWEEN ? AND ?", "#{date} 00:00:00", "#{date} 23:59:59")
   end
 end
