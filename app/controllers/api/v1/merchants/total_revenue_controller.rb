7560da0074a0a8acce91580a7e2e1b93f6c2ae18
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-26 21:44:52 -0600

    Refactor TotalRevenueController to use TotalRevenueSerializer

--- app/controllers/api/v1/merchants/total_revenue_controller.rb
+++ app/controllers/api/v1/merchants/total_revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::TotalRevenueController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Merchant.total_revenue(params[:date]))
+    render json: TotalRevenueSerializer.new(Merchant.total_revenue(params[:date]))
   end
 end
