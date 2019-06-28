82f6ce2c47629732881c776d63ea6e0fed9bb8cf
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-28 16:59:33 -0600

    Add specs for Item relationship endpoints

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -24,4 +24,35 @@ RSpec.describe "Items API" do
 
     expect(item["id"].to_i).to eq(id)
   end
+
+  it "delivers the InvoiceItems for a single Item" do
+    item            = create(:item)
+
+    invoice_item_1  = create(:invoice_item, item: item)
+    invoice_item_2  = create(:invoice_item, item: item)
+    invoice_item_3  = create(:invoice_item, item: item)
+
+    get "/api/v1/items/#{item.id}/invoice_items"
+
+    expect(response).to be_successful
+
+    item_invoice_items = JSON.parse(response.body)["data"]
+
+    expect(item_invoice_items[0]["id"].to_i).to eq(invoice_item_1.id)
+    expect(item_invoice_items[1]["id"].to_i).to eq(invoice_item_2.id)
+    expect(item_invoice_items[2]["id"].to_i).to eq(invoice_item_3.id)
+  end
+
+  it "delivers the Merchant for a single Item" do
+    merchant = create(:merchant)
+    item     = create(:item, merchant: merchant)
+
+    get "/api/v1/items/#{item.id}/merchant"
+
+    expect(response).to be_successful
+
+    item_merchant = JSON.parse(response.body)["data"]
+
+    expect(item_merchant["id"].to_i).to eq(merchant.id)
+  end
 end
