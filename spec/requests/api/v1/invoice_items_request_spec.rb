0f99e83c357e2eca2665ccf9bcfa01c81edbf848
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-28 16:37:01 -0600

    Add specs for InvoiceItem Item/Invoice relationship endpoints

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -24,4 +24,30 @@ RSpec.describe "InvoiceItem API" do
 
     expect(invoice_item["id"].to_i).to eq(id)
   end
+
+  it "delivers the Item for a single InvoiceItem" do
+    item          = create(:item)
+    invoice_item  = create(:invoice_item, item: item)
+
+    get "/api/v1/invoice_items/#{invoice_item.id}/item"
+
+    expect(response).to be_successful
+
+    invoice_item_item = JSON.parse(response.body)["data"]
+
+    expect(invoice_item_item["id"].to_i).to eq(item.id)
+  end
+
+  it "delivers the Invoice for a single InvoiceItem" do
+    invoice       = create(:invoice)
+    invoice_item  = create(:invoice_item, invoice: invoice)
+
+    get "/api/v1/invoice_items/#{invoice_item.id}/invoice"
+
+    expect(response).to be_successful
+
+    invoice_item_invoice = JSON.parse(response.body)["data"]
+
+    expect(invoice_item_invoice["id"].to_i).to eq(invoice.id)
+  end
 end
