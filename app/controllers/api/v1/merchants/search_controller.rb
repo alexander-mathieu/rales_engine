8056e7e4f653d4989cd53a224f537b247014adcd
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-29 11:29:58 -0600

    Add index action to Merchants SearchController

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::Merchants::SearchController < ApplicationController
+  def index
+    render json: MerchantSerializer.new(Merchant.search_all_by(search_params))
+  end
+
   def show
     render json: MerchantSerializer.new(Merchant.search_by(search_params))
   end
