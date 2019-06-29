2b2fe2e41c1a210f1544b838817fbd3de9b79272
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-28 22:26:31 -0600

    Add paid_invoice_ids class method to Invoice model with spec

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:invoice_items) }
     it { should have_many(:items).through(:invoice_items) }
   end
+
+  describe "instance methods" do
+    it "#paid_invoice_ids" do
+      invoice_1 = create(:invoice)
+      invoice_2 = create(:invoice)
+      invoice_3 = create(:invoice)
+      invoice_4 = create(:invoice)
+      invoice_5 = create(:invoice)
+      invoice_6 = create(:invoice)
+      invoice_7 = create(:invoice)
+      invoice_8 = create(:invoice)
+
+      create(:transaction, invoice: invoice_1)
+      create(:transaction, invoice: invoice_2)
+      
+      create(:failed_transaction, invoice: invoice_3)
+      create(:failed_transaction, invoice: invoice_4)
+      create(:failed_transaction, invoice: invoice_5)
+      create(:failed_transaction, invoice: invoice_6)
+      create(:failed_transaction, invoice: invoice_7)
+      create(:failed_transaction, invoice: invoice_8)
+
+      expect(Invoice.paid_invoice_ids).to eq([invoice_1.id, invoice_2.id])
+    end
+  end
 end
